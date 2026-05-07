3b72ba50c57c477ed99282a07b94e5855475d08a
Gadi-Satwika <satwikagadi2005@gmail.com>
2026-05-07 20:41:15 +0530

    Deployment Change in Production-1

--- server/index.js
+++ server/index.js
@@ -40,7 +40,8 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    // The (.*) tells Express to match any string and name it as a parameter
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
